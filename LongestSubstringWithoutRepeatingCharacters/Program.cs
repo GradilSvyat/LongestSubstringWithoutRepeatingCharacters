08c844d794e6f372c71423a8eef1126aef5dd98e
GradilSvyat <43150486+GradilSvyat@users.noreply.github.com>
2020-10-16 19:19:15 +0300

    Добавьте файлы проекта.

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] myStringArray = {
+                "abcabcbb",
+                "bbbbb",
+                "pwwkew",
+                "",
+                " " };
+            foreach (var s in myStringArray)
+            {
+                Console.WriteLine(LengthOfLongestSubstring(s));
+            }
+            Console.WriteLine("3,1,3,0,1");
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            List<char> res = new List<char>();
+            int result = 0;
+            foreach(char currenrChar in s)
+            {
+                    if (res.Contains(currenrChar))
+                    {
+                        res.RemoveRange(0,res.IndexOf(currenrChar) +1);
+                    }
+                res.Add(currenrChar);
+                if (result < res.Count)
+                {
+                    result = res.Count;
+                }
+            }
+            return result;
+        }
+    }
+}
